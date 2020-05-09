401ab98b25cfc231df65ae13f31c37ae996ecabd
Felipe Merchán <felipe-sc@hotmail.es>
2020-05-08 23:22:26 -0500

    Se optimizaron las imágenes y se añadíó una transición a .color

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ const imgContainer = document.querySelector('#imgContainer')
 const buyButton = document.querySelector('#buyButton')
 
 function changeData(image, alt, color) {
-  nikePhoto.src = image
+  nikePhoto.srcset = image
   nikePhoto.alt = alt
   imgContainer.style.backgroundColor = color
   buyButton.style.backgroundColor = color
@@ -16,27 +16,27 @@ function changeData(image, alt, color) {
 
 function changeTheme() {
   if (this.classList.contains('black')) {
-    const image = './src/img/black-nike.png'
+    const image = './src/img/black-nike.png, ./src/img/black-nike@2x.png 2x'
     const alt = 'Zapato deportivo Nike color negro, para correr.'
     const color = '#000'
     changeData(image, alt, color)
   } else if (this.classList.contains('green')) {
-    const image = './src/img/green-nike.png'
+    const image = './src/img/green-nike.png, ./src/img/green-nike@2x.png 2x'
     const alt = 'Zapato deportivo Nike color verde, para correr.'
     const color = '#0b8457'
     changeData(image, alt, color)
   } else if (this.classList.contains('blue')) {
-    const image = './src/img/blue-nike.png'
+    const image = './src/img/blue-nike.png, ./src/img/blue-nike@2x.png 2x'
     const alt = 'Zapato deportivo Nike color azul, para correr.'
     const color = '#5466b1'
     changeData(image, alt, color)
   } else if (this.classList.contains('orange')) {
-    const image = './src/img/orange-nike.png'
+    const image = './src/img/orange-nike.png, ./src/img/orange-nike@2x.png 2x'
     const alt = 'Zapato deportivo Nike color naranja, para correr.'
     const color = '#dd6b4d'
     changeData(image, alt, color)
   } else if (this.classList.contains('red')) {
-    const image = './src/img/red-nike.png'
+    const image = './src/img/red-nike.png, ./src/img/red-nike@2x.png 2x'
     const alt = 'Zapato deportivo Nike color rojo, para correr.'
     const color = '#eb4559'
     changeData(image, alt, color)
